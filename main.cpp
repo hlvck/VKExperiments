984717239d982dae23ea131c0e21f368ca1da641
hlvck <7290570+hlvck@users.noreply.github.com>
2022-09-07 12:42:19 +0100

    Logical Device and Queues

--- main.cpp
+++ main.cpp
@@ -61,6 +61,9 @@ class HelloTriangleApplication {
 		GLFWwindow* window;
 		VkInstance instance;
 		VkDebugUtilsMessengerEXT debugMessenger;
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		VkDevice device;
+		VkQueue graphicsQueue;
 
 		struct QueueFamilyIndices {
 			std::optional<uint32_t> graphicsFamily;
@@ -198,10 +201,49 @@ class HelloTriangleApplication {
 			createInstance();
 			setupDebugMessenger();
 			pickPhysicalDevice();
+			createLogicalDevice();
+		}
+
+		void createLogicalDevice() {
+			QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+			VkDeviceQueueCreateInfo queueCreateInfo{};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+			queueCreateInfo.queueCount = 1;
+
+			float queuePriority = 1.0f;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+
+			VkPhysicalDeviceFeatures deviceFeatures{};
+
+			VkDeviceCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+			createInfo.pQueueCreateInfos = &queueCreateInfo;
+			createInfo.queueCreateInfoCount = 1;
+
+			createInfo.pEnabledFeatures = &deviceFeatures;
+
+			createInfo.enabledExtensionCount = 0;
+
+			if (enableValidationLayers)
+			{
+				createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+				createInfo.ppEnabledLayerNames = validationLayers.data();
+			} else {
+				createInfo.enabledLayerCount = 0;
+			}
+
+			if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+			{
+				throw std::runtime_error("Failed to create logical device!");
+			}
+
+			vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
 		}
 
 		void pickPhysicalDevice() {
-			VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 			uint32_t deviceCount = 0;
 			vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
@@ -264,6 +306,8 @@ class HelloTriangleApplication {
 		}
 
 		void cleanup() {
+			vkDestroyDevice(device, nullptr);
+
 			if (enableValidationLayers)
 			{
 				DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
