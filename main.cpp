fe8895b914612fabd3a40ddffde14fd2735aa206
hlvck <7290570+hlvck@users.noreply.github.com>
2022-11-10 20:55:02 +0000

    Image views

--- main.cpp
+++ main.cpp
@@ -99,6 +99,7 @@ class HelloTriangleApplication {
 		std::vector<VkImage> swapChainImages;
 		VkFormat swapChainImageFormat;
 		VkExtent2D swapChainExtent;
+		std::vector<VkImageView> swapChainImageViews;
 
 		SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
 			SwapChainSupportDetails details;
@@ -302,6 +303,38 @@ class HelloTriangleApplication {
 			pickPhysicalDevice();
 			createLogicalDevice();
 			createSwapChain();
+			createImageViews();
+			createGraphicsPipeline();
+		}
+
+		void createGraphicsPipeline() {
+
+		}
+
+		void createImageViews() {
+			swapChainImageViews.resize(swapChainImages.size());
+
+			for (size_t i = 0; i < swapChainImages.size(); i++) {
+				VkImageViewCreateInfo createInfo{};
+				createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+				createInfo.image = swapChainImages[i];
+				createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+				createInfo.format = swapChainImageFormat;
+				createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+				createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+				createInfo.subresourceRange.baseMipLevel = 0;
+				createInfo.subresourceRange.levelCount = 1;
+				createInfo.subresourceRange.baseArrayLayer = 0;
+				createInfo.subresourceRange.layerCount = 1;
+
+				if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+					throw std::runtime_error("Failed to create image views.");
+				}
+			}
+
 		}
 
 		void createSwapChain() {
@@ -494,6 +527,9 @@ class HelloTriangleApplication {
 		}
 
 		void cleanup() {
+			for (auto imageView : swapChainImageViews) {
+				vkDestroyImageView(device, imageView, nullptr);
+			}
 			vkDestroySwapchainKHR(device, swapChain, nullptr);
 			vkDestroyDevice(device, nullptr);
 
